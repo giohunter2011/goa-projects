b0afd20057989af4e7a12a8bc8318346b45896fa
giphunter2011 <giorgirusishvili2011@gmail.com>
2025-07-03 20:12:50 -0700

    second commit

---
+++ lesson02/homework/homework.py
@@ -0,0 +1,20 @@
+print("black")
+print("sea food")
+print("king boxsing")
+
+name = "faster furies"
+name = "karate kid"
+name = "how to trane your dragon"
+name = "deadly football"
+name = "avatar"
+
+# yester day we lern about int,str and varieble
+
+name = "giorgi"
+old = 14
+
+print("my name is" + "name" + int + "old")
+
+
+
+
