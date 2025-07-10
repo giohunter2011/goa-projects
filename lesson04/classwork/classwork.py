736f53ef8297afa99308c25cbc0e6623c0568118
giphunter2011 <giorgirusishvili2011@gmail.com>
2025-07-10 09:21:36 -0700

    second commit

---
+++ lesson04/classwork/classwork.py
@@ -0,0 +1,14 @@
+inputexample = input("ღირს თუ არა ორი მონიტორის ყიდვა")
+
+print(inputexample)
+
+print("კი")
+
+
+
+inputexample = ("მუშაობს თუ არა ჩემი მაცივარი")
+print("კი")
+#updait
+inputexample = ("444")
+print(inputexample)
+
---
+++ lesson04/homework/homework.py
@@ -0,0 +1,18 @@
+inputexample = input("giorgi")
+outputexample = "hello user"
+print(inputexample)
+print(outputexample)
+
+#ინპუტი არის მაგალიტად როცა ვაწვებით მაუსს ის ხსნის კომპიუტერში რაღაცას ხოლო იმ რაღცას რასაც ჩვენ ვხედავტ ეკრანზე არის ოუტპუტი
+
+inputexample = input("hello")
+
+outputexample = "hello hello"
+print(inputexample)
+print(outputexample)
+
+inputexample = input("kargia cxovreba")
+print(inputexample)
+print("gasagebia")
+
+
